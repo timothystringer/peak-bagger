6dca14cb5e50dbcf9503ae68c26a0b001132ea33
Tim Stringer <timstringerwf1@blueyonder.co.uk>
2025-12-07 22:12:00 +0000

    Update meta title handling and refactor favicon asset links

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
     <head>
+        <title>{{ $title ?? config('app.name') }}</title>
         @include('partials.head')
     </head>
     <body class="min-h-screen bg-white dark:bg-zinc-800">
